e01fc75703ac16a7923fe2025ed35810d65541d8
Martin Kamau <martinkimu09@gmail.com>
2022-10-09 16:54:15 +0300

    Imported Home.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import UserGreeting from './UserGreeting';
+import Home from './Home';
 
 
 function App() {
   return(
     <div>
+      <Home />
       <UserGreeting />
     </div>
   )
