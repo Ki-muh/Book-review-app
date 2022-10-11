abd990625f44a59898014cd8b65dfb35c817f39e
Martin Kamau <martinkimu09@gmail.com>
2022-10-12 00:39:34 +0300

    Updated Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import './index.css';
+// import Home from "./Home";
 
 function NavBar() {
-    return (
-        <>
-        </>
-    )
+  return (
+    <nav className="nav">
+      <NavLink to="/" className="book-app">Classic Books</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/books">Books</NavLink>
+    </nav>
+  );
 }
 
 export default NavBar;
